051d8de4f6fd1bbcd35a09537d132c4932cf42ef
codepanthertechnologies <codepanthertechnologies@gmail.com>
2026-05-29 13:45:24 +0530

    17.0: init version

--- smart_duplicate_detection/__manifest__.py
+++ smart_duplicate_detection/__manifest__.py
@@ -23,7 +23,7 @@ Features:
 * Lightweight ORM queries
 * Production-ready architecture
     """,
-    "version": "19.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Tools",
     "license": "LGPL-3",
     "author": "Code Panther Technologies",
