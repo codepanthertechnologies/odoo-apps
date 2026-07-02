df68f19d01fb1aae3d2242b92b661a2463dd7768
Kritika lohani <lohanikritika975@gmail.com>
2026-07-02 17:40:30 +0000

    Bump version to 17.0.1.1.0 in manifest

--- smart_duplicate_detection/__manifest__.py
+++ smart_duplicate_detection/__manifest__.py
@@ -23,7 +23,7 @@ Features:
 * Lightweight ORM queries
 * Production-ready architecture
     """,
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "category": "Tools",
     "license": "LGPL-3",
     "author": "Code Panther Technologies",
